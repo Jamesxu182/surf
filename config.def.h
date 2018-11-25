ffb0d27b97e14f2b11a6ef704eb892b85df473d0
Yifan Xu <xuy7@tcd.ie>
2018-11-25 12:53:18 +0000

    Change shortcuts

--- config.def.h
+++ config.def.h
@@ -120,7 +120,7 @@ static WebKitFindOptions findopts = WEBKIT_FIND_OPTIONS_CASE_INSENSITIVE |
 		.v = (const char *[]){ "/bin/sh", "-c", \
 			"url=\"$(buku -p -f 3 " \
 			"| sed \'s/\t/ /g\' " \
-			"| dmenu -p $3 -i " \
+			"| dmenu -p $3 -i -l 10 " \
 			"| cut -d \' \' -f 1 " \
 			"| buku -f 1 -p " \
 			"| awk \'FNR==2 {print $2}\')\" && xprop -id $1 -f $2 8s -set $2 $url", \
@@ -148,6 +148,7 @@ static SiteSpecific certs[] = {
 };
 
 #define MODKEY GDK_CONTROL_MASK
+#define GDK_ALT_MASK GDK_MOD1_MASK
 
 /* hotkeys */
 /*
@@ -159,7 +160,7 @@ static Key keys[] = {
 	{ MODKEY,                GDK_KEY_g,      spawn,      SETPROP("_SURF_URI", "_SURF_GO", PROMPT_GO) },
 	{ MODKEY,                GDK_KEY_f,      spawn,      SETPROP("_SURF_FIND", "_SURF_FIND", PROMPT_FIND) },
 	{ MODKEY,                GDK_KEY_slash,  spawn,      SEARCH("_SURF_FIND", "_SURF_GO", PROMPT_SEARCH)},
-	{ MODKEY,                GDK_KEY_b,      spawn,      BOOKMARKS("_SURF_GO", PROMPT_BOOKMARKS)},
+	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_b,      spawn,      BOOKMARKS("_SURF_GO", PROMPT_BOOKMARKS)},
 
 
 	{ 0,                     GDK_KEY_Escape, stop,       { 0 } },
@@ -172,18 +173,21 @@ static Key keys[] = {
 	{ MODKEY,                GDK_KEY_h,      navigate,   { .i = -1 } },
 
 	/* vertical and horizontal scrolling, in viewport percentage */
-	{ MODKEY,                GDK_KEY_j,      scrollv,    { .i = +10 } },
-	{ MODKEY,                GDK_KEY_k,      scrollv,    { .i = -10 } },
-	{ MODKEY,                GDK_KEY_b,      scrollv,    { .i = +50 } },
-	{ MODKEY,                GDK_KEY_space,  scrollv,    { .i = -50 } },
-	{ MODKEY,                GDK_KEY_i,      scrollh,    { .i = +10 } },
-	{ MODKEY,                GDK_KEY_u,      scrollh,    { .i = -10 } },
+	// { GDK_MOD1_MASK,         GDK_KEY_j,      		scrollv,    { .i = +10 } },
+	// { GDK_MOD1_MASK,         GDK_KEY_k,      		scrollv,    { .i = -10 } },
+	// { MODKEY,                GDK_KEY_f,      		scrollv,    { .i = +50 } },
+	// { MODKEY,                GDK_KEY_b,  			scrollv,    { .i = -50 } },
+	// { GDK_MOD1_MASK,         GDK_KEY_h,      		scrollh,    { .i = +10 } },
+	// { GDK_MOD1_MASK,         GDK_KEY_semicolon,     scrollh,    { .i = -10 } },
+	{ MODKEY,                GDK_KEY_KP_Page_Down,	scrollv,    { .i = +50 } },
+	{ MODKEY,                GDK_KEY_KP_Page_Up,  	scrollv,    { .i = -50 } },
 
 
 //	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_j,      zoom,       { .i = -1 } },
 //	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_k,      zoom,       { .i = +1 } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_q,      zoom,       { .i = 0  } },
 	{ MODKEY,                GDK_KEY_minus,  zoom,       { .i = -1 } },
+	{ MODKEY,                GDK_KEY_plus,   zoom,       { .i = +1 } },
 	{ MODKEY,                GDK_KEY_equal,   zoom,       { .i = +1 } },
 
 	{ MODKEY,                GDK_KEY_p,      clipboard,  { .i = 1 } },
@@ -193,21 +197,21 @@ static Key keys[] = {
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_n,      find,       { .i = -1 } },
 
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_p,      print,      { 0 } },
-	{ MODKEY,                GDK_KEY_t,      showcert,   { 0 } },
+	// { MODKEY,                GDK_KEY_t,      showcert,   { 0 } },
 
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_a,      togglecookiepolicy, { 0 } },
-	{ 0,                     GDK_KEY_F11,    togglefullscreen, { 0 } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_o,      toggleinspector, { 0 } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_a,      togglecookiepolicy, { 0 } },
+	// { 0,                     GDK_KEY_F11,    togglefullscreen, { 0 } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_o,      toggleinspector, { 0 } },
 
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_c,      toggle,     { .i = CaretBrowsing } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_f,      toggle,     { .i = FrameFlattening } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_g,      toggle,     { .i = Geolocation } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_s,      toggle,     { .i = JavaScript } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_i,      toggle,     { .i = LoadImages } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_v,      toggle,     { .i = Plugins } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_b,      toggle,     { .i = ScrollBars } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_t,      toggle,     { .i = StrictTLS } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_m,      toggle,     { .i = Style } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_c,      toggle,     { .i = CaretBrowsing } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_f,      toggle,     { .i = FrameFlattening } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_g,      toggle,     { .i = Geolocation } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_s,      toggle,     { .i = JavaScript } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_i,      toggle,     { .i = LoadImages } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_v,      toggle,     { .i = Plugins } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_b,      toggle,     { .i = ScrollBars } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_t,      toggle,     { .i = StrictTLS } },
+	// { MODKEY|GDK_SHIFT_MASK, GDK_KEY_m,      toggle,     { .i = Style } },
 };
 
 /* button definitions */
